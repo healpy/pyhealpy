d10cc5b4360ce25f520303fb5067b3650b88e87b
Andrea Zonca <code@andreazonca.com>
2024-07-10 18:40:32 -0700

    simplify requirements, update test file

--- test_healpyvis.py
+++ test_healpyvis.py
@@ -1,7 +1,10 @@
 import healpy as hp
+import matplotlib.pyplot as plt
 
 m = hp.read_map(
-    "healpy/test/data/wmap_band_iqumap_r9_7yr_W_v4_udgraded32_masked_smoothed10deg_fortran.fits"
+    "test/data/wmap_band_iqumap_r9_7yr_W_v4_udgraded32_masked_smoothed10deg_fortran.fits"
 );
 
 hp.projview(m, coord=["G"], projection_type="mollweide");
+
+plt.savefig("map.png")
